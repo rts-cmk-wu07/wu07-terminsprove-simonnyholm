eea1b8297531eb22c6c8c91a1b8e67941485bde4
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-02-13 13:58:49 +0100

    add additional routes to app.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Layout from "./components/Layout";
 import Home from "./views/Home.jsx";
 import MySchedule from "./views/MySchedule.jsx";
 import Search from "./views/Search.jsx";
+import Login from "./views/Login";
+import Welcome from "./views/Welcome";
+import ClassDetails from "./views/ClassDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index path="/home" element={<Home />} />
+        <Route index element={<Welcome />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/myschedule" element={<MySchedule />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/classdetails" element={<ClassDetails />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
