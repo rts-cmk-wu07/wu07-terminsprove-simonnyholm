5b7d31c535d50d73dc4b08f26f7f0af7244fd1ea
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-02-13 14:00:29 +0100

    add get call

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const Home = () => {
+  var [content, setContent] = useState();
+
+  useEffect(
+    function () {
+      axios
+        .get("http://localhost:4000/api/v1/classes", {})
+        .then((response) => setContent(response));
+    },
+    [setContent]
+  );
+
+  console.log("erf", content);
   return (
     <div>
       <h1>Home</h1>
+      
     </div>
   );
 };
