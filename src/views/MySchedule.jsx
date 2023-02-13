8c55f71546aacfcae25259ae1cabddc7e2833105
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-02-13 12:39:58 +0100

    add compulsory routes

---
+++ src/views/MySchedule.jsx
@@ -0,0 +1,9 @@
+const MySchedule = () => {
+  return (
+    <div>
+      <h1>My Schedule</h1>
+    </div>
+  );
+};
+
+export default MySchedule;
---
+++ src/views/Welcome.jsx
@@ -0,0 +1,14 @@
+const Welcome = () => {
+  return (
+    <div>
+      <section>
+        <h1>Believe yourself</h1>
+        <p>Train like a pro</p>
+      </section>
+      <section>
+        <button>Get Started</button>
+      </section>
+    </div>
+  );
+};
+export default Welcome;
