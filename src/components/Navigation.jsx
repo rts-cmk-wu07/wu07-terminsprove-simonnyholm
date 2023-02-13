71bdbd99f1dedd80c22dc49b5cdcad06551b6daa
Simon Nyholm Sørensen <simonnyholm@htomail>
2023-02-13 12:39:22 +0100

    add navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+const Navigation = () => {
+  const [navClicked, setNavClicked] = useState(false);
+
+  return (
+    /*
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+    */
+
+    <>
+      {!navClicked && (
+        <button
+          onClick={(event) => {
+            setNavClicked(true);
+          }}
+        >
+          Menu
+        </button>
+      )}
+
+      {navClicked && (
+        <div>
+          <button
+            onClick={(event) => {
+              setNavClicked(false);
+            }}
+          >
+            Close
+          </button>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/home">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/search">Search</NavLink>
+              </li>
+              <li>
+                <NavLink to="/myschedule">MySchedule</NavLink>
+              </li>
+              <li>
+                <button>Ternary logon/Out</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navigation;
